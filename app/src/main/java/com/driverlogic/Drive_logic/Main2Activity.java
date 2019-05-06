89f4d3ceebb74a45f5320523c9da6646d1260460
pavloZel9 <pavloZel9@gmail.com>
2019-05-06 20:26:54 +0300

    navigation action+frgmnet

--- app/src/main/java/com/driverlogic/Drive_logic/Main2Activity.java
+++ app/src/main/java/com/driverlogic/Drive_logic/Main2Activity.java
@@ -1,8 +1,13 @@
 package com.driverlogic.Drive_logic;
 
+import android.app.FragmentTransaction;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,13 +18,31 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Main2Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    SharedPreferences sPref;
+    String id,name,email,pic;
+    AccessToken mAccessToken;
+    ImageView icon_myinfo;
 
+    FragmentTransaction fTrans;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAccessToken = AccessToken.getCurrentAccessToken();
+        getUserProfile(mAccessToken);
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -31,6 +54,10 @@ public class Main2Activity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+        Reg_layout1 frag1=new Reg_layout1();
+        fTrans = getFragmentManager().beginTransaction();
+        fTrans.add(R.id.frgmCont, frag1);
+        fTrans.commit();
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -38,6 +65,7 @@ public class Main2Activity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -54,6 +82,19 @@ public class Main2Activity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main2, menu);
+        icon_myinfo= findViewById(R.id.icon_myinfo);
+
+
+        Toast.makeText(this, pic, Toast.LENGTH_SHORT).show();
+        Picasso.with(Main2Activity.this)
+                .load(pic)
+                .placeholder(R.drawable.com_facebook_button_icon)
+                .error(R.drawable.com_facebook_auth_dialog_cancel_background)
+                .into(icon_myinfo);
+        TextView t1=findViewById(R.id.name_myinfo);
+        t1.setText(name);
+        TextView t2=findViewById(R.id.email_myinfo);
+        t2.setText(email);
         return true;
     }
 
@@ -79,14 +120,17 @@ public class Main2Activity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
-            // Handle the camera action
+            Reg_layout1 frag2=new Reg_layout1();
+            FragmentTransaction fTrans1 = getFragmentManager().beginTransaction();
+            fTrans1.replace(R.id.frgmCont, frag2);
+            fTrans1.commit();
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_tools) {
-
-        } else if (id == R.id.nav_share) {
+        }  else if (id == R.id.nav_share) {
+            Reg_layout2 frag3=new Reg_layout2();
+            FragmentTransaction  fTrans2 = getFragmentManager().beginTransaction();
+            fTrans2.replace(R.id.frgmCont, frag3);
+            fTrans2.commit();
 
         } else if (id == R.id.nav_send) {
 
@@ -95,5 +139,31 @@ public class Main2Activity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    private void getUserProfile(AccessToken currentAccessToken) {
+        GraphRequest request = GraphRequest.newMeRequest(
+                currentAccessToken,
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        try {
+                            //You can fetch user info like this…
+                            pic=object.getJSONObject("picture").getJSONObject("data").getString("url");
+                            name=object.getString("name");
+                            email=object.getString("email");
+                            id=object.getString("id");
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,email,picture.width(200)");
+        request.setParameters(parameters);
+        request.executeAsync();
     }
 }
---
+++ app/src/main/java/com/driverlogic/Drive_logic/Reg_layout2.java
@@ -0,0 +1,31 @@
+package com.driverlogic.Drive_logic;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class Reg_layout2 extends Fragment {
+    final String LOG_TAG = "myLogs";
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_reg_layout2, null);
+
+        Button button = (Button) v.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.e(LOG_TAG, "Button click in Fragment2");
+            }
+        });
+
+        return v;
+    }
+}
